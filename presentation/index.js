4bd1add3c23829e1098f50b9e70f6d64efb0a31b
Mathyba <emuck@tutanota.com>
2018-06-11 22:28:52 +0200

    Structure of Ken Wheeler Keynote done

--- presentation/index.js
+++ presentation/index.js
@@ -8,6 +8,8 @@ import {
   Deck,
   Heading,
   ListItem,
+  Image,
+  Appear,
   List,
   Quote,
   Slide,
@@ -15,7 +17,7 @@ import {
 } from "spectacle";
 
 // Import theme
-import createTheme from "spectacle/lib/themes/default";
+import createTheme from 'spectacle-theme-nova';
 
 // Require CSS
 require("normalize.css");
@@ -30,6 +32,10 @@ const theme = createTheme({
   secondary: "Helvetica"
 });
 
+const images = {
+  ken: require('../assets/kenwheeler.jpg'),
+};
+
 export default class Presentation extends React.Component {
   render() {
     return (
@@ -43,28 +49,98 @@ export default class Presentation extends React.Component {
           </Text>
         </Slide>
         <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Heading 1</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
+          <Image src={images.ken} width="25%"/>
+          <Heading size={6} fit caps lineHeight={1} textColor="primary">
+            Ken Wheeler
+          </Heading>
+          <Text size={6} textColor="secondary">Director of Open Source at Formidable, creator of libraries such as Spectacle, urql, react-music, react-game-kit and Webpack Dashboard.</Text>
         </Slide>
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
+          <Heading size={4} fit textColor="primary">
+            Quoi de neuf chez ReactJs ?
+          </Heading>
+          <Text size={6} textColor="secondary">V16.3</Text>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <Heading size={6} textColor="secondary" fit caps>Strict Mode</Heading>
           <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
+            <Appear>
+              <ListItem>Uniquement en mode developpement</ListItem>
+            </Appear>
+            <Appear>
+              <ListItem>Prévient de l’utilisation par l’API d’une ancienne chaîne de référence</ListItem>
+            </Appear>
+            <Appear>
+              <ListItem>détecte les effets secondaires inattendus</ListItem>
+            </Appear>
+            <Appear>
+              <ListItem>La suite?</ListItem>
+            </Appear>
           </List>
         </Slide>
         <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
+          <Heading size={6} margin="0 0 3rem" textColor="secondary" fit caps>Nouvelle gestion des lifecycle</Heading>
+          <Appear>
+            <Heading textColor="primary" size={4}>
+              Les méthodes bientôt dépréciées :
+            </Heading>
+          </Appear>
+          <List>
+            <Appear>
+              <ListItem>componentWillMount</ListItem>
+            </Appear>
+            <Appear>
+              <ListItem>componentWillReceiveProps</ListItem>
+            </Appear>
+            <Appear>
+              <ListItem>componentWillUpdate</ListItem>
+            </Appear>
+          </List>
+          <Appear>
+            <Heading textColor="primary" size={4}>
+              Les nouvelles méthodes :
+            </Heading>
+          </Appear>
+          <List>
+            <Appear>
+              <ListItem>getDerivedStateFromProps</ListItem>
+            </Appear>
+            <Appear>
+              <ListItem>getSnapshotBeforeUpdate</ListItem>
+            </Appear>
+          </List>
+        </Slide>
+        <Slide>
+          <Heading size={6} textColor="secondary" fit caps>
+            Context API : Nouvelle gestion du state global ?
+          </Heading>
+          <List>
+            <Appear>
+              <ListItem>Gestion des transferts de props entre enfant/parent sans intermediaire.</ListItem>
+            </Appear>
+            <Appear>
+              <ListItem>Creation du context ⇒ provider => consumer</ListItem>
+            </Appear>
+            <Appear>
+              <ListItem>Context vs Redux ?</ListItem>
+            </Appear>
+            <Appear>
+            <ListItem>DEMO</ListItem>
+            </Appear>
+          </List>
+        </Slide>
+        <Slide>
+          <Heading size={6} textColor="secondary" fit caps>
+            Suspense
+          </Heading>
+          <List>
+            <Appear>
+              <ListItem>Nouvelle gestion du rendu asynchrone</ListItem>
+            </Appear>
+            <Appear>
+              <ListItem>DEMO</ListItem>
+            </Appear>
+          </List>
         </Slide>
       </Deck>
     );
